4df4032e7074460b170481fa40f0638639f2d127
Juan Clavijo <jclavijomartinez@gmail.com>
2022-04-16 19:00:14 -0500

    gestion obras

--- src/ControlGaleria.java
+++ src/ControlGaleria.java
@@ -1,5 +1,6 @@
 public class ControlGaleria {
     
     public static void main(String[] args) {
+        
     }
 }
--- src/GestionObras.java
+++ src/GestionObras.java
@@ -1,3 +1,5 @@
+import java.util.Collection;
+import java.util.List;
 public class GestionObras{
-
+    
 }
