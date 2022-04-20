32c064e572e1b07a63af56a96d0db04d44b06e61
Juan Clavijo <jclavijomartinez@gmail.com>
2022-04-20 16:07:46 -0500

    prueba de recibo de push

--- src/PantallaGaleria.java
+++ src/PantallaGaleria.java
@@ -1,5 +1,5 @@
 import java.util.*;
-//comentrio para probar push
+//comentrio para probar push ed JSCM
 public class PantallaGaleria {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
