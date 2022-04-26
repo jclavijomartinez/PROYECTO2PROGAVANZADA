d63d62934d3d2fd5ed5924c746a05fbfe40dd371
Juan Clavijo <jclavijomartinez@gmail.com>
2022-04-25 22:39:19 -0500

    commit xd

--- src/GestionClientes.java
+++ src/GestionClientes.java
@@ -5,18 +5,18 @@ public class GestionClientes {
     private GestionObras gestionObras;
     private Cliente clientes;
     public List<Cliente> crearClientes() {
-        List<Cliente> listaclientes = new ArrayList<>();
+        List<Cliente> Listaclientes = new ArrayList<>();
         Cliente cliente1 = new Cliente(9876l, 1000365419l, "Brayan ", "Estivenson", "cra 143 #22-56a", 3192399712l);
-        listaclientes.add(cliente1);
+        Listaclientes.add(cliente1);
         Cliente cliente2 = new Cliente(2314l, 1000785419l, "Juan", "perez", "cra 151 #26-90", 3214180084l);
-        listaclientes.add(cliente2);
+        Listaclientes.add(cliente2);
         Cliente cliente3 = new Cliente(5768l, 1000369019l, "Andres ", "Perdomo", "cra 34 #45-5", 3112674330l);
-        listaclientes.add(cliente3);
+        Listaclientes.add(cliente3);
         Cliente cliente4 = new Cliente(1039l, 1000365423l, "Esneyder", "Centanaro", "cra 7 #444-666", 3203589556l);
-        listaclientes.add(cliente4);
+        Listaclientes.add(cliente4);
         Cliente cliente5 = new Cliente(1888l, 1560365419l, "Snyder ", "Zach", "cra 5 #22-65", 3134245722l);
-        listaclientes.add(cliente5);
+        Listaclientes.add(cliente5);
 
-        return listaclientes;
+        return Listaclientes;
     }
 }
