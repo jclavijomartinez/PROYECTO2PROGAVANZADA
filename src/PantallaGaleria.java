c2b7ab84aebeabb325c76f0ab709d6d6c066a0fa
Juan Clavijo <jclavijomartinez@gmail.com>
2022-04-26 04:18:49 -0500

    commit x

--- src/PantallaGaleria.java
+++ src/PantallaGaleria.java
@@ -16,7 +16,7 @@ public class PantallaGaleria {
                 System.out.println(" 3. Insertar Obrar ");
                 System.out.println(" 4. Modificar Obra ");
                 System.out.println(" 5. Eliminar Obra ");
-                System.out.println(" 6. Ver lsitado de Clientes registrados en el sistema");
+                System.out.println(" 6. Ver listado de Clientes registrados en el sistema");
                 System.out.println(" 7. Buscar un Cliente ");
                 System.out.println(" 8. Insertar Cliente ");
                 System.out.println(" 9. Modificar los datos del Cliente ");
@@ -81,7 +81,7 @@ public class PantallaGaleria {
                                 year = scanner.nextInt();
                                 System.out.println("");
                                 for (int i = 0; i < listaobrasdisponibles.size(); i++) {
-                                    if (/*listaobrasdisponibles.get(i).getFecha(). == year      TRABAJARLE*/) {
+                                    if (true/*listaobrasdisponibles.get(i).getFecha(). == year      TRABAJARLE*/) {
                                         System.out.println("[" + i + "] " + "Titulo: "+ listaobrasdisponibles.get(i).getTitulo() + " fecha de creacion: "+ /* listaobrasdisponible.get(i).getFecha()+ */" precio de referencia: "+ listaobrasdisponibles.get(i).getPrecioRef() + " COP, Dimensiones: "+ listaobrasdisponibles.get(i).getDimensiones());
                                     }
                                 }
@@ -91,22 +91,61 @@ public class PantallaGaleria {
                         }
                         break;
                     case 3:
+                    Calendar nuevafecha = Calendar.getInstance();
                         try (Scanner  nuevaobra = new Scanner(System.in)) {
-                            Long codigoObraNueva = Scanner
-                            System.out.print("Dame el codigo de la obra: tiene que tener 7 digitos");
+                            System.out.print("Dame el codigo de la nueva obra (tiene que tener 7 digitos): ");
+                            for (int i = 0; i < args.length; i++) {
+                                
+                            }
+                            Long codigoObraNueva = scanner.nextLong();
+                            System.out.println("");
+                            System.out.print("Dame el titulo de la nueva obra: ");
+                            String tituloObraNueva = scanner.nextLine();
+                            System.out.println("");
+                            System.out.print("Dame el dia de creacion de la nueva obra: ");
+                            int diaObraNueva = scanner.nextInt();
+                            System.out.println("");
+                            System.out.print("Dame el mes de creacion de la nueva obra: ");
+                            int mesObraNueva = scanner.nextInt();
+                            System.out.println("");
+                            System.out.print("Dame el year de creacion de la nueva obra: ");
+                            int yearObraNueva = scanner.nextInt();
+                            System.out.println("");
+                            nuevafecha.set(yearObraNueva,mesObraNueva,diaObraNueva);
+                            System.out.print("Dame el precio de la nueva obra: ");
+                            Float precioObraNueva = scanner.nextFloat();
+                            System.out.println("");
+                            System.out.print("Dame las dimensiones de la nueva obra: (altoxancho) ");
+                            String dimsObraNueva = scanner.nextLine();
+                            System.out.println("");
+                            List<Obra> listaobras = new ArrayList<>();
+                            List<Artista> listartistas = new ArrayList<>();
+                            Obra obranueva = new Obra(codigoObraNueva, tituloObraNueva, nuevafecha, precioObraNueva, dimsObraNueva, true);
+                            GestionObras obras1 = new GestionObras();
+                            listaobras = obras1.crearobras();
+                            listaobras.add(obranueva);
+                            listartistas = obras1.crearartistas();
+                            for (int i = 0; i < listartistas.size(); i++) {
+                                System.out.println("falta listar");
+                            }
+                            
+                            
                         } catch (Exception e) {
-                            //TODO: handle exception
+                            
                         }
                         break;
                     case 4:
-
+                        
                         break;
                     case 5:
                         try {
                             System.out.print("Digite el codigo de la obra: ");
                             codigo = scanner.nextInt();
+                            System.out.println("");
                             System.out.print("Digite el codigo de compra de la obra ");
                             codigoCompra = scanner.nextInt();
+                            System.out.println("");
+                            GestionObras test1 = new GestionObras();
                             test1.EliminarObra(codigo, test1.crearobras(), codigoCompra);
                         } catch (Exception e) {
                             System.out.print("El codigo no existe");
@@ -120,52 +159,83 @@ public class PantallaGaleria {
                         Listaimprimir = lista.crearClientes();
                         for (int i = 0; i < Listaimprimir.size(); i++) {
 
-                            System.out.print("Cedula: " + Listaimprimir.get(i).getCedula() + "Nombre Completo: "
+                            System.out.println("Cedula: " + Listaimprimir.get(i).getCedula() + " Nombre Completo: "
                                     + Listaimprimir.get(i).getNombre() + Listaimprimir.get(i).getApellidos()
-                                    + "Direccion: " + Listaimprimir.get(i).getDireccionEntrega() + "Telefono: "
+                                    + " Direccion: " + Listaimprimir.get(i).getDireccionEntrega() + " Telefono: "
                                     + Listaimprimir.get(i).getTelefono());
                         }
                         break;
                     case 7:
-                        try {
+                    try {
                             String nombre1, apellido1, direccion1;
                         long Cedula1, Telefono1, CodigoC1;
-                        GestionClientes obra;
+                        GestionClientes obra = new GestionClientes();
                         Cliente nuevoCliente;
-                            System.out.print("Informacion nuevo cliente");
+                            System.out.print("Informacion nuevo cliente: ");
                             
-                            System.out.print("Ingrese la cedula del cliente");
+                            System.out.print("Ingrese la cedula del cliente: ");
                             Cedula1 = scanner.nextLong();
-                            System.out.print("Ingrese el nombre del cliente");
+                            System.out.println("");
+                            System.out.print("Ingrese el nombre del cliente:");
                             nombre1 = scanner.nextLine();
-                            System.out.print("Ingrese el apellido del cliente");
+                            System.out.println("");
+                            System.out.print("Ingrese el apellido del cliente:");
                             apellido1 = scanner.nextLine();
-                            System.out.print("Ingrese la direccion del cliente");
+                            System.out.println("");
+                            System.out.print("Ingrese la direccion del cliente:");
                             direccion1 = scanner.nextLine();
-                            System.out.print("Ingrese el telefono del cliente");
+                            System.out.println("");
+                            System.out.print("Ingrese el telefono del cliente:");
                             Telefono1 = scanner.nextLong();
-                            Cliente nuevocliente = new Cliente(CodigoC1, Cedula1, nombre1, apellido1, direccion1, Telefono1);
-                            List<Cliente> lcliente = new ArrayList<>();
-                            lcliente = obra.crearClientes();
-                            lcliente.add(nuevoCliente);
-                            System.out.print("Ingrese el codigo del cliente ");
+                            System.out.println("");
+                            System.out.print("Ingrese el codigo del cliente: ");
                             CodigoC1 = scanner.nextLong();
+                            List<Cliente> lcliente = new ArrayList<>();
+                             lcliente = obra.crearClientes(); 
+                            //lcliente.add(nuevoCliente);
                                 for (int i = 0;i< lcliente.size(); i++){
                                     if(CodigoC1==codigo)
                                     {
-                                        System.out.println("Codigo de cliente existente");
+                                        System.out.println("Codigo de cliente existente"); 
 
+                                    }
+                                    else
+                                    {
+                                        System.out.println("Codigo Disponible");
                                     }
 
                                 }
+                            
+                            Cliente nuevocliente = new Cliente(CodigoC1, Cedula1, nombre1, apellido1, direccion1, Telefono1);
+                           
                         } catch (Exception e) {
                             System.out.println("Me vale vrga");//cambia a otra cosa xd 
 
                             
                         }
 
+                        
                         break;
                     case 8:
+                            try {
+                                Long CodModificar;
+                                 System.out.println("Ingrese el codigo del cliente a modificar ");
+                                 CodModificar = scanner.nextLong();
+                                 if (CodModificar!=codigo)
+                                 {
+                                     System.out.print("El codigo no exite");
+                                     break;
+                                 }
+                                 else{
+                                     for (int i = 0; i < Listaimprimir.size(); i++) {
+                                         
+                                     }
+                                 }
+                            } catch (Exception e) {
+                                //TODO: handle exception
+                            }
+                           
+
 
                         break;
 
